8b8e9649a73133f82d846cf6d00fee81d5853631
Benjahoff <oriozabalabenja@gmail.com>
2020-10-07 17:49:53 -0300

    Boostrap y formato

--- Web/my-app/src/App.js
+++ Web/my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import UserDetails from './components/UserDetails';
+
 function App() {
   return (
     <div className="App">
--- Web/my-app/src/components/userDetails.js
+++ Web/my-app/src/components/userDetails.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
+import '../styles/main.css';
+import Button from 'react-bootstrap/Button';
 class UserDetails extends Component {
     render() {
       return (
        <div>
-           <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+           <Button variant="danger">Danger</Button>
         </div>
       );
     }
 }
 export default UserDetails;
-
-   
