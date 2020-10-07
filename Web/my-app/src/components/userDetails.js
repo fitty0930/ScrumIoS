9dd134ed1fb03f90571e8582aa1d01a01543fb57
Benjahoff <oriozabalabenja@gmail.com>
2020-10-07 16:59:45 -0300

    Agregue boostrap

---
+++ Web/my-app/src/components/userDetails.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class userDetails extends Component {
+    render() {
+      return (
+            <h1>ajskldajskld</h1>
+      );
+    }
+  }
+   
+  export default userDetails;
