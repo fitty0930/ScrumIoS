15b2136129b390d6c2190b4701a5b0d836356d1b
Tomas <tomasporcaro22@gmail.com>
2020-10-07 17:14:49 -0300

    Comentarios

--- Web/back-end/server.js
+++ Web/back-end/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+//configuraciones del servidor
 app.use(bodyParser.json());
 app.use(cors());
 app.use(
@@ -12,7 +13,7 @@ app.use(
     })
 )
 const mongoURI = 'mongodb+srv://scrumuser123:scrum123@cluster0.vtmxc.mongodb.net/scrumGame?retryWrites=true&w=majority';
-mongoose.connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true }) // Conexion con la base de datos
     .then(() => console.log('MongoDB Conectado: ' + mongoURI.substr(0, 7)))
     .catch(err => console.log(err));
 app.listen(port, () => {
