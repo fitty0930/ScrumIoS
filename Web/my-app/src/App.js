1c88edfdcb4cf2dffccdd21a65f99421df7d7018
Benjahoff <oriozabalabenja@gmail.com>
2020-10-14 17:14:03 -0300

    Template hecho, a la espera del back con datos

--- Web/my-app/src/App.js
+++ Web/my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {BrowserRouter as Router,Route,Link} from "react-router-dom";
 import './App.css';
 import UserDetails from './components/UserDetails';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <UserDetails />
+      <UserDetails />    {/* /id */}
     </div>
   );
 }
