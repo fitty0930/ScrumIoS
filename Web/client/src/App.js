830b01cf02afc81a45962f72c0b8eec28a3f7446
ErasunN <nicoerasun98@gmail.com>
2020-10-29 10:29:25 -0300

    Lista de usuarios en espera, falta funcionalidad de los botones

--- Web/client/src/App.js
+++ Web/client/src/App.js
@@ -21,13 +21,22 @@ function App() {
       {uid:7, name:"Cassoli", puntos:7000, mail:"Mail012@testing.com"},
       {uid:8, name:"Nico", puntos:7000,    mail:"Mail654@testing.com"},
   ]
+  const waitingUsers = [
+      {uid:1, name:"Benja1", puntos:12000,  mail:"Mail123@testing.com"},
+      {uid:2, name:"Martin2", puntos:11000, mail:"Mail456@testing.com"},
+      {uid:3, name:"Tomas3", puntos:10000, mail:"Mail789@testing.com"},
+      {uid:4, name:"Juan4", puntos:9000,    mail:"Mail234@testing.com"},
+      {uid:5, name:"Franco5", puntos:8000,  mail:"Mail567@testing.com"},
+      {uid:6, name:"Joel6", puntos:7000,    mail:"Mail890@testing.com"},
+      {uid:7, name:"Cassoli7", puntos:7000, mail:"Mail012@testing.com"},
+      {uid:8, name:"Nico8", puntos:7000,    mail:"Mail654@testing.com"},
+  ]
   return (
     <Router>
         <Route exact path='/adminUsers' render={() => (
               <div>
                 <Filtrar filterValue={filterValue} setFilterValue={setFilterValue}/>
-                <ListaUsers users={users} filterValue={filterValue}/>
-                
+                <ListaUsers users={users} waitingUsers={waitingUsers} filterValue={filterValue}/>
               </div>
         )}/>
        <Route exact path='/login' component={Login} />
--- Web/client/src/components/ListaUser.js
+++ Web/client/src/components/ListaUser.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './AdminStyles.css';
-export function ListaUsers({users, filterValue}){
+export function ListaUsers({users, waitingUsers, filterValue}){
     const filteredUsers = users.filter(entry => entry.name.toLowerCase().includes(filterValue.toLowerCase())
     || entry.mail.toLowerCase().includes(filterValue.toLowerCase()))
+    const filteredWaitingUsers = waitingUsers.filter(entry => entry.name.toLowerCase().includes(filterValue.toLowerCase())
+    || entry.mail.toLowerCase().includes(filterValue.toLowerCase()))
+    
+    //Funcion de visualizacion/cambio entre lista de usuarios y usuarios en espera de aprobacion
+    setTimeout(() => {
+        document.querySelector("#btn-toggle").addEventListener("click", ()=>{
+            document.querySelector("#listaUser").toggleAttribute("hidden")
+            document.querySelector("#listaUserEspera").toggleAttribute("hidden")
+            document.querySelector("#addOnEspera").toggleAttribute("hidden")
+        })
+    }, 2000);
 
     return(
         <div>
         {console.log(users)}
-            <div className="col-10 mx-auto contenedorListaUser overflow-auto">
+            <div className="col-10 mx-auto contenedorListaUser overflow-auto" id="listaUser">
+                <div className="sticky-top rounded-pill m-1 h-50 d-inline-block">
+                    <h1 className="text-black p-1">Lista de Usuarios:</h1>
+                </div>
                 <ul className="list-group" id="lista-usuarios">
                     {filteredUsers.map((user) => (
                         <div key={user.uid}>
@@ -26,6 +40,34 @@ export function ListaUsers({users, filterValue}){
                         </div>
                     ))}
                 </ul>
+            </div>
+            <div className="col-10 mx-auto my-2 contenedorListaUser overflow-auto" id="listaUserEspera" hidden>
+                <div className="sticky-top rounded-pill m-1 h-50 d-inline-block">
+                    <h1 className="text-black p-1">Lista de Usuarios <span className="font-weight-bold">en espera</span>:</h1>
+                </div>
+                <ul className="list-group" id="lista-usuarios">
+                    {filteredWaitingUsers.map((user) => (
+                        <div key={user.uid}>
+                            <li className="list-group-item d-flex justify-content-between align-items-center input-color m-1 rounded-pill">
+                                <div className="col-8">
+                                    {user.name}  
+                                </div>
+                                <div className="position-left align-items-left">
+                                    {user.mail}  
+                                </div>
+                                <Link to={"/user/"+user.uid}>
+                                    <button type="button" className="btn btn-color rounded-pill btn-admin-user" id={user.uid}>Aceptar</button>
+                                </Link>
+                                <Link to={"/user/"+user.uid}>
+                                    <button type="button" className="btn btn-color rounded-pill btn-admin-user" id={user.uid}>Rechazar</button>
+                                </Link>
+                            </li>
+                        </div>
+                    ))}
+                </ul>
+            </div>
+            <div className="col-10 mx-auto my-1">
+                <button type="button" id="btn-toggle" className="btn btn-primary float-right">Ver usuarios <span id="addOnEspera">en espera</span></button>
             </div>
         </div>
     )
